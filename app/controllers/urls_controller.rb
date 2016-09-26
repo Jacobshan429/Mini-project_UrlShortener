d18a1d0d65e57cdb2bb5877dea2a86c6eb730f84
jacobshan <jacobshan429@gmail.com>
2016-09-26 19:45:49 +0800

    almost done

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,29 @@
+class UrlsController < ApplicationController
+  def show
+    @url = Url.find_by( :short_url => params[:short_url] )
+    redirect_to @url.original_url
+  end
+
+  def index
+    @urls = Url.all
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(params_url)
+    require 'securerandom'
+    @url.short_url = SecureRandom.hex(3)
+    @url.save
+    redirect_to urls_path
+
+  end
+
+  private
+
+  def params_url
+    params.require(:url).permit(:short_url, :original_url)
+  end
+
+
+
+end
